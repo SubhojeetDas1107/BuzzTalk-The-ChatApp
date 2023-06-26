30af4ef3647cf7f4f0d1c32a94a3c68e6dc4d275
Subhojeet Das <80981317+SubhojeetDas1107@users.noreply.github.com>
2023-06-27 02:12:18 +0530

    Update appwriteConfig.js

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,9 +1,9 @@
 import { Client, Account, Databases } from 'appwrite';
 
 export const API_ENDPOINT = 'https://cloud.appwrite.io/v1'
-export const PROJECT_ID = '6499f4b43803c4178229'
-export const DATABASE_ID = '6499f521d05086eeb716'
-export const COLLECTION_ID_MESSAGES = '6499f5343b4ffafd892d'
+export const PROJECT_ID = '6499f7878d1f2fd21da7'
+export const DATABASE_ID = '6499f7a9aa5c656716df'
+export const COLLECTION_ID_MESSAGES = '6499f7b5a4289801d6bb'
 
 const client = new Client()
     .setEndpoint(API_ENDPOINT) 
